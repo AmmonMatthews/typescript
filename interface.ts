8eb68c6f6e6c0fbaf0421af53078b3d39c2d6b2b
AmmonMatthews <ammonmatthews@gmail.com>
2020-08-18 10:09:49 -0600

    typescript with classes

--- interface.ts
+++ interface.ts
@@ -7,3 +7,15 @@ const user: User = {
     name: "Ammon", 
     id: 2
 }
+
+class UserAccount {
+    name: string;
+    id: number;
+
+    constructor(name: string, id: number) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+const new_user = new UserAccount("Wendey", 4)
