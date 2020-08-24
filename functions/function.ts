9ca50e145f8aa9d05ba7986fc26e3f86b043461d
AmmonMatthews <ammonmatthews@gmail.com>
2020-08-24 10:25:08 -0600

    committing everything

--- functions/function.ts
+++ functions/function.ts
@@ -2,6 +2,7 @@ function myName(firstName: string, lastName: string) : string {
     return firstName + lastName
 }
 
+//This is epxlicitly declaring that the type inline with the function and variable. 
 let myName2: (first: string, last: string) => string = function (first: string, last: string): string{
     return first + last
 }
